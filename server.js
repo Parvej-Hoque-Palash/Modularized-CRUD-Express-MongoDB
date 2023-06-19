de617e8accbc363396d333630a31a53274a5b822
Parvej-Hoque-Palash <cloudnumber981@gmail.com>
2023-06-19 20:02:23 +0600

    Added crud operations

--- server.js
+++ server.js
@@ -1,83 +1,117 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const nodemon = require('nodemon')
-const mongoose = require('mongoose')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const nodemon = require("nodemon");
+const mongoose = require("mongoose");
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-const uri = process.env.MONGODB_URI
-mongoose.connect(uri, {useNewUrlParser: true})
-  .then(() => console.log('Connected!'))
+const uri = process.env.MONGODB_URI;
+mongoose
+  .connect(uri, { useNewUrlParser: true })
+  .then(() => console.log("Connected!"));
 
-mongoose.connection.on('connected', function (){
-    console.log('Mongoose default connection open');
-})
+mongoose.connection.on("connected", function () {
+  console.log("Mongoose default connection open");
+});
 
 //If the connection throws an error
-mongoose.connection.on('error', function (err){
-    console.log('Mongoose default connection error');
-})
-//API to check connection
-app.get('/', (req, res) =>{
-    res.json({message: 'Welcome to our app'})
-})
+mongoose.connection.on("error", function (err) {
+  console.log("Mongoose default connection error");
+});
 
-let users = []
-let lastId = 0
+const userSchema = new mongoose.Schema(
+  {
+    fname: String,
+    lname: String,
+    email: String,
+    age: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+//API to check connection
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to our app" });
+});
 
 //API to create user
-app.post('/users', (req, res) => {
-    const user = req.body
-    user.id = ++lastId
-    users.push(user)
-    res.status(201).json(user)
-})
+app.post("/users", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something is wrong" });
+  }
+});
 
 //API to get users
-app.get('/users', (req, res) =>{
-    res.json(users)
-})
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.json(users);
+  } catch (error) {
+    res.status(404).json({ message: "User not found" });
+  }
+});
 
 //API to get users by id
-app.get('/users/:id', (req, res) =>{
-    const id = req.params.id
-    const user = users.find((u) => u.id == id)
-    if(user){
-        res.json(user)
-    }else{
-        res.status(404).json({message: 'User not found'})
+app.get("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await User.findById(id);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
     }
-})
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something is wrong" });
+  }
+});
 
 //API to edit user info
-app.put('/users/:id', (req, res) => {
-    const id = req.params.id
-    const body = req.body
-    const user = users.find((u) => u.id == id)
-    if(user){
-        user.fname = body.fname
-        user.lname = body.lname
-        res.json(user)  
-    }else{
-        res.status(404).json({message: 'User not found'})
+app.put("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const body = req.body;
+    const user = await User.findByIdAndUpdate(id, body, { new: true });
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
     }
-})
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something is wrong" });
+  }
+});
 
 //API to delete user
-app.delete('/users/:id', (req, res) => {
-    const id = req.params.id
-    const userIndex = users.findIndex((u) => u.id == id)
-    if(userIndex){
-        users.splice(userIndex,1)
-        res.json(users)
-    }else{
-        res.status(404).json({message: 'User not found'})
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await User.findByIdAndDelete(id);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
     }
-})
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something is wrong" });
+  }
+});
 
-const port = process.env.PORT
-app.listen(port, () =>{
-    console.log(`Server is running on port ${port}`)
-})
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
