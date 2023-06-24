8d5e9e273db396eae40fc5abe4b3b8762ebedcd6
Parvej-Hoque-Palash <cloudnumber981@gmail.com>
2023-06-24 09:51:38 +0600

    added accessToken and refreshToken in CRUD

--- server.js
+++ server.js
@@ -34,9 +34,32 @@ const userSchema = new mongoose.Schema(
   {
     timestamps: true
   }
-)
+  )
+  
+  const User = mongoose.model("User", userSchema);
+  
+  //Middleware to authenticate JWT access token
+  const authenticateToken = (req, res, next) =>{
+    const authHeader = req.headers.authorization
+    const token = authHeader && authHeader.split(' ')[1]
+  //token is an array where its first element(index:0) is 'Bearer' and 2nd element(index:1) is the 'given token'
+  if(!token){
+    //if token is found then it is authorized, else not.
+    res.status(401).json({message: 'Unauthorized!'})
+    return
+  }else {
+    jwt.verify(token, process.env.JWT_SECRET, (err, payload) =>{
+      if(err){
+        res.status(401).json({message: 'Unauthorized!'})
+      }else{
+        req.user = payload //user taking user details where payload = user email and pass
+        next()
+      } 
+    } )
+  }
+}
 
-const User = mongoose.model("User", userSchema);
+//app.use(authenticateToken);
 
 //API to check connection
 app.get("/", (req, res) => {
@@ -67,18 +90,14 @@ app.post("/users", async (req, res) => {
 
 app.post('/users/login', async (req, res) => {
   try {
-    const {email, password} = req.body
-    const user = await User.findOne({email: email})
-    //Checking if user email is valid
-    if(!user){
-      res.status(401).json({ message: "User not found" });
+    const {email, password, type, refreshToken} = req.body
+    if(!type){
+      res.status(401).json({message: 'Type is not found'})
     }else{
-      //Checking if user password is valid
-      const isValidPassword = await bcrypt.compare(password, user.password)
-      if(!isValidPassword){
-        res.status(401).json({ message: "Wrong Password!" });
+      if(type == 'email'){
+        await handleEmailLogin(email, res, password);
       }else{
-        res.status(200).json(user);
+        handleRefreshLogin(refreshToken, res);
       }
     }
   } catch (error) {
@@ -87,8 +106,40 @@ app.post('/users/login', async (req, res) => {
   }
 })
 
+//Get a user profile
+app.get('/profile', authenticateToken, async (req, res) =>{
+  try {
+    const id = req.user.id; //used 'user' instead of 'params'
+    const user = await User.findById(id);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something is wrong" });
+  }
+})
+
+//Get a user profile by id(Not Working yet)
+app.get('/profile/:id',authenticateToken, async (req, res) => {
+  try {
+    const id = req.user.id;
+    const user = await User.findById(id);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something is wrong" });
+  }
+});
+
 //API to get users
-app.get("/users", async (req, res) => {
+app.get('/users', async (req, res) => {
   try {
     const users = await User.find({});
     res.json(users);
@@ -98,7 +149,7 @@ app.get("/users", async (req, res) => {
 });
 
 //API to get users by id
-app.get("/users/:id", async (req, res) => {
+app.get('/users/:id', async (req, res) => {
   try {
     const id = req.params.id;
     const user = await User.findById(id);
@@ -116,11 +167,17 @@ app.get("/users/:id", async (req, res) => {
 //API to edit user info
 app.put("/users/:id", async (req, res) => {
   try {
+    //keeping the hash password in database after edit also.
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(req.body.password, salt)
+    const password = hash
     const id = req.params.id;
     const body = req.body;
     const user = await User.findByIdAndUpdate(id, body, { new: true });
     if (user) {
+      user.password = password
       res.json(user);
+      user.save()
     } else {
       res.status(404).json({ message: "User not found" });
     }
@@ -129,7 +186,7 @@ app.put("/users/:id", async (req, res) => {
     res.status(500).json({ message: "Something is wrong" });
   }
 });
-
+//require('crypto').randomBytes(64).toString('hex')
 //API to delete user
 app.delete("/users/:id", async (req, res) => {
   try {
@@ -150,3 +207,48 @@ const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+function handleRefreshLogin(refreshToken, res) {
+  if (!refreshToken) {
+    res.status(401).json({ message: 'refreshToken is not defined' });
+  } else {
+    jwt.verify(refreshToken, process.env.JWT_SECRET, async (err, payload) => {
+      if (err) {
+        res.status(401).json({ message: 'Unauthorized' });
+      } else {
+        const id = payload.id;
+        const user = await User.findById(id);
+        if (!user) { //if user is not found
+          res.status(401).json({ message: 'Unauthorized' });
+        } else { //if user is found
+          getUserTokens(user, res);
+        }
+      }
+    });
+  }
+}
+
+async function handleEmailLogin(email, res, password) {
+  const user = await User.findOne({ email: email });
+  //Checking if user email is valid
+  if (!user) {
+    res.status(401).json({ message: "User not found" });
+  } else {
+    //Checking if user password is valid
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      res.status(401).json({ message: "Wrong Password!" });
+    } else {
+      getUserTokens(user, res);
+    }
+  }
+}
+
+function getUserTokens(user, res) {
+  const accessToken = jwt.sign({ email: user.email, id: user._id }, process.env.JWT_SECRET, { expiresIn: '1m' });
+  const refreshToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '5m' });
+  const userObj = user.toJSON();
+  userObj['accessToken'] = accessToken;
+  userObj['refreshToken'] = refreshToken;
+  res.status(200).json(userObj);
+}
+
